4b7e4f45e30b1f21933c82b59780888b94b0cc1e
Erik Berthold <eberthold@users.noreply.github.com>
2023-05-15 07:18:13 +0200

    added  first dotfiles

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+Invoke-Expression (&starship init powershell)
+Set-PSReadLineOption -PredictionViewStyle ListView
+# oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
+# Import-Module -Name Terminal-Icons
