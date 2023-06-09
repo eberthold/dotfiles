1b2c3d4e205a31439e13449eaa5248bd1028038a
Erik Berthold <eberthold@users.noreply.github.com>
2023-06-09 21:35:17 +0200

    improved powershell profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,11 @@
 Invoke-Expression (&starship init powershell)
 Set-PSReadLineOption -PredictionViewStyle ListView
+
+# fix blue bg color for folders in ls command
+$PSStyle.FileInfo.Directory = ""
+
+# make mklink available in powershell
+function mklink { cmd /c mklink $args }
+
 # oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
 # Import-Module -Name Terminal-Icons
